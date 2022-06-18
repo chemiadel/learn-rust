6481f3537aa6315298d2f1374d8be57046f7ca58
chemiadel <pace3man@gmail.com>
2022-06-18 09:41:03 +0100

    Working with string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let str = "aabaa";
+    let reversed  = str.chars().rev().collect::<String>();
+
+    println!("{}", reversed);
+
+    // return concat!(firstname, lastname)
 }
